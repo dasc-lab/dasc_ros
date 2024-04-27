d2be581c47df595ed6d9970c12dae101bec1364c
kalebbennaveed <kalebbennaveed@gmail.com>
2024-04-27 15:16:32 -0400

    udp decentralised code added

--- dasc_ros_utils/scripts/publish_setpoint.py
+++ dasc_ros_utils/scripts/publish_setpoint.py
@@ -12,6 +12,7 @@ import time
 import matplotlib.pyplot as plt
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
+import socket
 
 received_x=[]
 received_y=[]
@@ -22,7 +23,17 @@ class SetpointAssignerNode(Node):
         self.publisher = self.create_publisher(TrajectorySetpoint, '/px4_1/fmu/in/trajectory_setpoint', 10)
         # self.marker_pub=self.create_publisher(Marker,'path_marker',10)
         # qos_profile = QoSProfile(reliability=QoSProfile.ReliabilityPolicy.RELIABLE)
-        self.timer = self.create_timer(0.1, self.publish_text)
+
+        # Now, adding the UDP code to decentralize communication between robots
+        self.UDP_IP = "192.168.1.149"
+        self.UDP_PORT_SEND_2 = 5006
+        self.UDP_PORT_SEND_3 = 5007
+        self.UDP_PORT_RECEIVE = 5005
+
+        self.sock_receive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+        self.sock_receive.bind((self.UDP_IP, self.UDP_PORT_RECEIVE))
+
+        self.timer = self.create_timer(0.065, self.publish_text)
         # self.subscription = self.create_subscription(VehicleLocalPosition,'/px4_1/fmu/out/vehicle_local_position',self.listener_callback,10)
         self.subscription = self.create_subscription(VehicleLocalPosition,'/px4_1/fmu/out/vehicle_local_position',self.listener_callback,QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
         # self.subscription
@@ -31,13 +42,40 @@ class SetpointAssignerNode(Node):
         
 
         self.msg = TrajectorySetpoint()
-        self.num_points=400
+        self.num_points=2200 #2000 to make it work
         self.angles=np.linspace(0,2*np.pi,self.num_points)
         # # Shape 8
-        self.x=np.cos(self.angles)
-        self.y=np.sin(2*self.angles)
+        # Adding required offset
+        self.x=1.5*np.cos(self.angles)
+        self.x=self.x+1.0
+        self.y=1.5*np.sin(2*self.angles)
+        # Reverse the direction
+        self.x=self.x[::-1]
+        self.y=self.y[::-1]
+        # changing the starting point
+
+        # Split the array into four quarters
+        q11 = self.x[:len(self.x)//4]
+        q12 = self.x[len(self.x)//4:2*len(self.x)//4]
+        q13 = self.x[2*len(self.x)//4:3*len(self.x)//4]
+        q14 = self.x[3*len(self.x)//4:]
+        # Concatenate the quarters in the desired order
+        self.x = np.concatenate((q14, q11, q12, q13))
+
+        # Split the array into four quarters
+        q21 = self.y[:len(self.y)//4]
+        q22 = self.y[len(self.y)//4:2*len(self.y)//4]
+        q23 = self.y[2*len(self.y)//4:3*len(self.y)//4]
+        q24 = self.y[3*len(self.y)//4:]
+        # Concatenate the quarters in the desired order
+        self.y = np.concatenate((q24, q21, q22, q23))
+
+
+        print("Reference x: ",self.x)
+        print("Reference y: ",self.y)
         # print(type(self.x))
-        self.v=0.5
+        self.v=0.75
+        self.vw=0.10
         # msg = TrajectorySetpoint()
         # num_points=200
         # angles=np.linspace(0,2*np.pi,num_points)
@@ -65,48 +103,46 @@ class SetpointAssignerNode(Node):
         # Any spline
         # x=
         self.count=0
-        # count=0
-        # Need to look into time syncronization in the future: Rahul
-
-        # msg.timestamp = self.get_clock().now().to_msg()
-
-        # msg = Pose()
-        # while rclpy.ok():
-        #     msg.position=[x[count], y[count], -1.0]
-        #     msg.velocity=[v,v,v]
-        #     time.sleep(0.1)
-        #     count+=1
-        #     print(count)
-        #     if(count==num_points):
-        #         msg.velocity=[0.0,0.0,0.0]
-        #         break
-
-        #     self.publisher.publish(msg)
-        #     self.get_logger().info('Publishing setpoint')
-
-        # print("Endpoint Reached")
+        
 
     def publish_text(self):
         if self.count < self.num_points:
             self.msg.position = [self.x[self.count], self.y[self.count], -0.16]
-            self.msg.velocity = [self.v, self.v, self.v]
+            self.msg.velocity = [self.v, self.v, self.vw]
             self.publisher.publish(self.msg)
             self.get_logger().info('Publishing setpoint %d' % self.count)
             self.count += 1
+
+            # Adding code for broadcasting coordinates
+            message_send = "(1,1) from robot 1"
+            sock_send_2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+            sock_send_2.sendto(message_send.encode(), ("192.168.1.132", self.UDP_PORT_SEND_2))
+            sock_send_2.close()
+
+            # sock_send_3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+            # sock_send_3.sendto(message_send.encode(), (UDP_IP, UDP_PORT_SEND_3))
+            # sock_send_3.close()
+
+            data, addr = self.sock_receive.recvfrom(1024)  # buffer size is 1024 bytes
+            print("Received message:", data.decode())
         else:
             self.msg.velocity = [0.0, 0.0, 0.0]
             self.publisher.publish(self.msg)
             self.get_logger().info('Endpoint Reached')
-            print(type(received_x))
-            print(received_x)
+            # print(type(received_x))
+            # print(received_x)
             cvt_received_x=np.asarray(received_x)
             cvt_received_y=np.asarray(received_y)
-            print(cvt_received_x)
+            # print(cvt_received_x)
             plt.plot(cvt_received_x,cvt_received_y,'r')
+            plt.grid()
             plt.show()
             plt.savefig("dummy_name_1.png")
             print("Image saved")
-            self.timer.cancel()  # Stop the timer when all points are published
+            print("Size received: ",len(cvt_received_x))
+            print("Size published: ", len(self.x))
+            # self.timer.cancel()  # Stop the timer when all points are published
+            self.count=0
 
 
     def listener_callback(self, msg):
